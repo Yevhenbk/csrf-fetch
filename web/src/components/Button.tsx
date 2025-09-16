a0823a8669c520cbe3176dc9d8c1dc33da4fe852
Yevhenbk <balagutrak.jenia@gmail.com>
2025-09-17 01:41:14 +0200

    Refactor Button component for improved readability and consistency in prop definitions

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,30 +1,30 @@
 import React from "react";
 
 interface ButtonProps {
-  children: React.ReactNode,
-  onClick?: React.MouseEventHandler<HTMLButtonElement>,
-  arialLabel: string,
-  type: "submit" | "reset" | "button" | undefined
-};
+  children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  arialLabel: string;
+  type: "submit" | "reset" | "button" | undefined;
+}
 
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
   arialLabel,
-  type
+  type,
 }) => {
   return (
-    <button 
+    <button
       type={type}
       onClick={onClick}
       aria-label={arialLabel}
-      className="bg-blue-700 text-white font-medium
-      text-lg py-2 px-6 rounded-md hover:bg-blue-800
-      w-[17.25rem]"
+      className="bg-white/15 backdrop-blur-md border border-white/20 px-6 py-3 rounded-full 
+      font-semibold text-lg flex items-center gap-2 shadow-lg w-[17.25rem] flex justify-center 
+      hover:bg-white/20 transition"
     >
       {children}
     </button>
-  )
+  );
 };
 
 export default Button;
